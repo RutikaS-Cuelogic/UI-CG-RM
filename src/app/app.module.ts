dc3b911b833e19e707aa932c3709320d59ac7478
“rutikashetye” <rutikashetye101@gmail.com>
2022-10-19 12:26:56 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+// primeng modules
+import {InputTextModule} from 'primeng/inputtext';
+import {Card, CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    
+    InputTextModule,
+    CardModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
